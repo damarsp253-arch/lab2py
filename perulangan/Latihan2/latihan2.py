c64a3a773b63632ff6a59ac72e104a87490e5f65
damarsp253-arch <damarsp253@gmail.com>
2025-10-27 20:06:44 +0700

    README

--- perulangan/Latihan2/latihan2.py
+++ perulangan/Latihan2/latihan2.py
@@ -1,9 +1,13 @@
-# Latihan 2: Bilangan acak < 0.5
-n = int(input("Masukkan jumlah n: "))
-count = 0
+import random
 
-while count < n:
-    angka = random.random(1)
-    if angka < 0.5:
+# Meminta input jumlah bilangan
+n = int(input("Masukkan jumlah n: "))
+
+i = 0  # inisialisasi penghitung
+while i < n:
+    angka = random.random()  # menghasilkan angka acak antara 0.0 - 1.0
+    if angka < 0.5:          # hanya menampilkan angka yang lebih kecil dari 0.5
         print(angka)
-        count += 1
+        i += 1               # hanya dihitung jika memenuhi syarat
+# program selesai 
+# latihan4.py
